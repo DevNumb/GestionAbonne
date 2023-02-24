83eebd1bf832ccba1dc7318562ec0359bc1f7058
DevNumb <96944608+DevNumb@users.noreply.github.com>
2023-02-24 01:03:52 +0100

    notifications

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,10 @@ import {Navbar,Nav,Container, NavDropdown}  from 'react-bootstrap';
 import {Link} from  'react-router-dom';
 import {Button} from 'react-bootstrap';
 import {useNavigate} from 'react-router-dom';
-import NavbarOffcanvas from 'react-bootstrap/esm/NavbarOffcanvas';
-import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import {BsFacebook} from 'react-icons/bs';
 import Media from 'react-media';
+import {IoNotifications} from 'react-icons/io5';
+import {GrStatusGoodSmall} from 'react-icons/gr';
 
 function Header(){
   const navigate = useNavigate();
@@ -23,14 +24,42 @@ let user = JSON.parse(localStorage.getItem('user-info'));
   localStorage.getItem('user-info') ?
   (user.options === 'Admin' || user.options === 'User') &&
   <>
+  <Media query="(min-width:600px)">
+      {matches =>{
+        return matches ?
+      <>
   <Navbar.Brand href="#">Welcome</Navbar.Brand>
   </>
-  : 
+  :
   <>
-  
-  
-  <Navbar.Brand href="#">Home</Navbar.Brand>
+   <Navbar.Brand href="#">Welcome</Navbar.Brand>
+   
+   <div style={{ position: 'relative',marginLeft:"150px"}}>
+      <GrStatusGoodSmall style ={{color:"red",position: 'absolute',
+          top: '3px',
+          right: '-1px',fontSize:"0.5rem"
+ }}> </GrStatusGoodSmall>
+      <IoNotifications style={{color:'white',marginTop:"1px"}}></IoNotifications>
+      </div>
+
+  </>;}}
+  </Media>
   </>
+  : 
+  <Media query="(min-width:600px)">
+  {matches =>{
+    return matches ?
+  <>
+  <Navbar.Brand href="#">Home</Navbar.Brand>
+ 
+  </>
+  :
+  <>
+    <Navbar.Brand href="#">Home</Navbar.Brand>
+   <BsFacebook  style={{  marginLeft:"200px",marginRight:"60px",fontSize:"1.4rem"}}></BsFacebook>
+  </>
+  ;}}
+  </Media>
 }
           
             <Navbar.Toggle aria-controls="Basic-navbar-nav"/>
@@ -45,11 +74,20 @@ let user = JSON.parse(localStorage.getItem('user-info'));
       {matches =>{
         return matches ?
       <>
-        <Link to="/GestionUtilisateur" style={{textDecoration: "none", color: "white", margin: "30px"}}>GestionUtilisateur</Link>
-        <Link to="/Evenement" style={{textDecoration: "none", color: "white", margin: "30px"}}>Evenement</Link>
-        <Link to="/Historique" style={{textDecoration: "none", color: "white", margin: "30px"}}>Historique</Link>
-        <Link to="/telechargmentpdf" style={{textDecoration: "none", color: "white", margin: "30px"}}>telechargmentpdf</Link>
-        <Link to="/conventions" style={{textDecoration: "none", color: "white", margin: "30px"}}>conventions</Link>
+        <Link to="/GestionUtilisateur" style={{textDecoration: "none", color: "white", margin: "20px"}}>GestionUtilisateur</Link>
+        <Link to="/Evenement" style={{textDecoration: "none", color: "white", margin: "20px"}}>Evenement</Link>
+        <Link to="/Historique" style={{textDecoration: "none", color: "white", margin: "20px"}}>Historique</Link>
+        <Link to="/telechargmentpdf" style={{textDecoration: "none", color: "white", margin: "20px"}}>telechargmentpdf</Link>
+        <Link to="/conventions" style={{textDecoration: "none", color: "white", margin: "20px"}}>conventions</Link>
+      <div style={{ position: 'relative',marginLeft:"270px",marginTop:"20px"}}>
+
+      <GrStatusGoodSmall style ={{color:"red",position: 'absolute',
+          top: '3px',
+          right: '-1px',fontSize:"0.5rem"
+ }}> </GrStatusGoodSmall>
+      <IoNotifications style={{color:'white',marginTop:"2px"}}></IoNotifications>
+      
+      </div>
       </>
       :
       <>
@@ -58,6 +96,7 @@ let user = JSON.parse(localStorage.getItem('user-info'));
       <Link to="/Historique" style={{textDecoration: "none", color: "white", marginBottom: "15px"}}>Historique</Link>
       <Link to="/telechargmentpdf" style={{textDecoration: "none", color: "white", marginBottom: "15px"}}>telechargmentpdf</Link>
       <Link to="/conventions" style={{textDecoration: "none", color: "white", marginBottom: "15px"}}>conventions</Link>
+
     </>;}}
     </Media>
     ) : user.options === 'User' ? (
@@ -67,11 +106,20 @@ let user = JSON.parse(localStorage.getItem('user-info'));
       <>
         <Link to="/AffichageEvent" style={{textDecoration: "none", color: "white", margin: "30px"}}>AffichageEvent</Link>
         <Link to="/AffichageConvention" style={{textDecoration: "none", color: "white", margin: "30px"}}>AffichageConventions</Link>
+        <div style={{ position: 'relative',marginLeft:"600px",marginTop:"30px"}}>
+      <GrStatusGoodSmall style ={{color:"red",position: 'absolute',
+          top: '3px',
+          right: '-1px',fontSize:"0.5rem"
+ }}> </GrStatusGoodSmall>
+      <IoNotifications style={{color:'white'}}></IoNotifications>
+      
+      </div>
       </>
       :
       <>
         <Link to="/AffichageEvent" style={{textDecoration: "none", color: "white", marginBottom: "15px",marginTop:"6px"}}>AffichageEvent</Link>
-        <Link to="/AffichageConvention" style={{textDecoration: "none", color: "white", marginBottom: "15px",}}>AffichageConventions</Link>
+        <Link to="/AffichageConvention" style={{textDecoration: "none", color: "white", marginBottom: "15px"}}>AffichageConventions</Link>
+   
       </>;
     }}
     </Media>
@@ -89,12 +137,13 @@ let user = JSON.parse(localStorage.getItem('user-info'));
           <Link to="/ContactUs" style={{textDecoration: "none", color: "white", padding: "15px", marginRight:"10px"}}>
            ContactUs
           </Link>
-        
+          <BsFacebook  style={{  marginTop:"15px",marginLeft:"60px",marginRight:"60px",fontSize:"1.6rem"}}></BsFacebook>
           <Link to="/Login" style={{textDecoration: "none", color: "white", padding: "8px" , marginRight:"15px"}}>
             <Button style={{border: "1 solid black", backgroundColor: "#03045e"}}>Login</Button>
           </Link>
           <Link to="/Register" style={{textDecoration: "none", color: "white", padding: "8px", marginRight:"15px"}}>
             <Button style={{border: "1 solid black", backgroundColor: "#03045e"}}>SignUp</Button>
+           
           </Link>
           </>
       :
@@ -105,7 +154,6 @@ let user = JSON.parse(localStorage.getItem('user-info'));
       <Link to="/ContactUs" style={{textDecoration: "none", color: "white", padding: "3px", marginBottom:"10px"}}>
        ContactUs
       </Link>
-    
       <Link to="/Login" style={{textDecoration: "none", color: "white", padding: "3px" , marginBottom:"10px"}}>
         Login
       </Link>
@@ -129,7 +177,7 @@ let user = JSON.parse(localStorage.getItem('user-info'));
  
           <Nav>
   {localStorage.getItem('user-info') &&
-    <NavDropdown title={user && user.username}>
+    <NavDropdown  style={{marginTop:"5px"}}title={user && user.username}>
       <NavDropdown.Item onClick={logOut}>Logout</NavDropdown.Item>
     </NavDropdown>
   }
