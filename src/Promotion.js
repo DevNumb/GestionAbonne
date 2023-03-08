339f64c3b449c647cd69c146d71094b77ad04dd6
DevNumb <96944608+DevNumb@users.noreply.github.com>
2023-03-08 13:20:45 +0100

    adding promotion page

--- src/Promotion.js
+++ src/Promotion.js
@@ -2,32 +2,24 @@ import {Button,Table}from 'react-bootstrap';
 import {useState,useEffect} from 'react';
 
 function Promotion(){
+  const [data1 , setData1] = useState([]);
     async function fetchData1(){
         let result = await fetch ("http://localhost:8000/api/listPromo");
        result = await result.json();
       setData1(result);
       } 
     
-      const [data1 , setData1] = useState([]);
+   
       useEffect ( ()=> {
      
     fetchData1();
     
     },[])
+    
     const [nom_promo,setNom_promo] = useState();
     const [file,setFile] = useState();
     
-    async function delEvents(id){
     
-    let result = await fetch ("http://localhost:8000/api/delEvent/" + id , {
-       method:"DELETE"
-    
-    });
-    
-    
-    result = await result.json();
-    fetchData1();
-    }
     async function addEvents(){
     const formData = new FormData();
     formData.append('nom_promo',nom_promo);
@@ -37,13 +29,21 @@ function Promotion(){
     body: formData
     });
     alert("Enregistrer");
-    
-    
     fetchData1();
+    let result2 = await fetch ("http://localhost:8000/api/sendOffer");
+   
     }
-    
-     
-    
+    async function delPromo(id){
+
+      let result = await fetch ("http://localhost:8000/api/delPromo/" + id , {
+         method:"DELETE"
+      
+      });
+      
+      
+      result = await result.json();
+      fetchData1();
+      }
     
     
     
@@ -77,16 +77,16 @@ function Promotion(){
           </thead>
           <tbody>
           {data1.map((item) => (
-      <tr key={item.id_Promo}>
-        <td>{item.id_Promo}</td>
-        <td>{item.nom_Promo}</td>
+      <tr key={item.id_promo}>
+        <td>{item.id_promo}</td>
+        <td>{item.nom_promo}</td>
         <td style={{display:"flex" , flexDirection:"row",justifyContent:"space-between"}}>
-          <a href ={"http://localhost:8000/" + item.Promo_img}  download={item.Promo_img} >
+          <a href ={"http://localhost:8000/" + item.promo_img}  download={item.Promo_img} >
             <Button class="primary" style={{marginRight:"5px"}}  >Telechargement</Button> </a> 
            
             </td>
             <td>
-            <Button variant="danger"   onClick={() => delEvents(item.id_event)}>Supprimer</Button>   
+            <Button variant="danger"  onClick={() => delPromo(item.id_promo)} >Supprimer</Button>   
     
             </td>
       </tr>
@@ -97,7 +97,7 @@ function Promotion(){
           
     
       );
-}
+             
 
-
+          }
 export default Promotion;
